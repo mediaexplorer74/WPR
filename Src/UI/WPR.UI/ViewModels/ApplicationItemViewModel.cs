768c043662f4ed488013622a8bcac954447da9a9
Andrey Zakharov <mediaexplorer74@hotmail.com>
2025-06-19 13:21:22 +0300

    wpr-avalonia 0.0.10 checkpoint

--- Src/UI/WPR.UI/ViewModels/ApplicationItemViewModel.cs
+++ Src/UI/WPR.UI/ViewModels/ApplicationItemViewModel.cs
@@ -1,8 +1,12 @@
-﻿using Avalonia.Media.Imaging;
+using Avalonia.Media.Imaging;
 using System.IO;
+using System.Threading.Tasks;
+using ReactiveUI;
+using System;
 
 using WPR.Models;
 using WPR.Common;
+using System.Reactive;
 
 namespace WPR.UI.ViewModels
 {
@@ -14,12 +18,23 @@ namespace WPR.UI.ViewModels
         public int IconSize => 90;
         public int Height => 160;
 
+        public ReactiveCommand<Unit, Unit> RunAppCommand { get; }
+        public ReactiveCommand<Unit, Unit> UninstallAppCommand { get; }
+
+        // Event that will be triggered when app needs to be uninstalled
+        public event EventHandler<ApplicationItemViewModel>? UninstallRequested;
+
         public ApplicationItemViewModel(Application app)
         {
             _App = app;
+            RunAppCommand = ReactiveCommand.Create(() => RunApp());
+            UninstallAppCommand = ReactiveCommand.Create(() => UninstallApp());
         }
 
         internal Application App => _App;
+        
+        // Property to expose the application model for easier access
+        public Application Model => _App;
 
         public string? Name => _App.Name;
         public string? Tooltip
@@ -50,6 +65,18 @@ namespace WPR.UI.ViewModels
 
                 return _Icon;
             }
+        }
+
+        private void RunApp()
+        {
+            // Logic to run the application
+            WPR.UI.ApplicationLaunchRequest.Ask(_App);
+        }
+
+        private void UninstallApp()
+        {
+            // Trigger the uninstall event so the parent view model can handle it
+            UninstallRequested?.Invoke(this, this);
         }
     }
 }
